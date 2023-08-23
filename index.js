21ffd0c63523c637497f12890d920d8547022b43
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2023-08-23 17:14:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log( "aiman kahn");
+
+// console.log(a)
+// let b="harry";
+// console.log(b)
+// ..........................
+// diffrence between var /let/constant/////////////////
+{
+    let b="this is my"
+    console.log(b)
+}
+// /////////////////////////////////////////////////////////////////////////////
+// primitive data types//////
+// nn ss bb u
+let a = null;
+let b = 254;
+let c = symbol;
+let d = string;
+let e = Boolean;
+let f = blignt;
+let g = undefined;
+
